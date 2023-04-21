327015c9d671a05caf4e16e0e609df1f659443e5
Ziming Gong <gong.zim@northeastern.edu>
2023-04-21 01:47:16 -0700

    update DI and DBcontext

---
+++ HRAPI/HRAPI/Controllers/CandidateController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApplicationCore.Contracts.Services;
+using Infrastructure.Service;
+
+
+namespace HRAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var candidate = _candidateService.GetCandidateByIdAsync(id);
+            return Ok(candidate);
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
--- HRAPI/HRAPI/Program.cs
+++ HRAPI/HRAPI/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿using ApplicationCore.Contracts.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Repositories;
+using ApplicationCore.Contracts.Services;
+using Infrastructure.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RecruitingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RecruitingDb"));
+});
+
+builder.Services.AddScoped<IJobCategoryRepository, JobCategoryRepository>();
+builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
+builder.Services.AddScoped<ICandidateService, CandidateService>();
+builder.Services.AddScoped<IEmployeeTypeServices, EmployeeTypeService>();
+builder.Services.AddScoped<IEmployeeTypeRepository, EmployeeTypeRepository>();
+
 
 var app = builder.Build();
 
